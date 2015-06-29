b63a0ad5490c47b07e4cea95c44938c4b86a1492
burrly <neilson.ks@gmail.com>
2015-06-29 16:58:12 -0700

    physics tests

---
+++ EctoplazaNeilsonProject/Assets/__Scripts/_ControlScripts/LineDrawerTest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineDrawerTest : MonoBehaviour {
+
+	LineRenderer lineRender; 
+	Transform[] allChildren;
+	// Use this for initialization
+	void Start () 
+	{
+		lineRender = this.GetComponent<LineRenderer>();
+
+		allChildren = GetComponentsInChildren<Transform>();
+
+		//print(allChildren[1]);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		int childCount = allChildren.Length ; 
+
+		lineRender.SetVertexCount(childCount);
+
+		for(int i = 0; i < childCount; i++)
+		{
+			if(i==0)
+			{
+				lineRender.SetPosition(i, allChildren[i+1].transform.position); 
+			}
+			else
+			{
+				lineRender.SetPosition(i, allChildren[i].transform.position); 
+			}
+		}
+
+	}
+}
