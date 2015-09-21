a386bf90908d1a0ff33ecd9322fb9dbfedcd2d24
chorgan93 <chorgan93@gmail.com>
2015-09-21 11:42:00 -0700

    Found the right one?
    Plz ignore these

---
+++ ECTOPLAZA/Assets/__Scripts/__SystemsScripts/CurrentModeS.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CurrentModeS : MonoBehaviour {
+
+	public static int currentMode = 0; // 0 = Ectoplasm, 1 = Stock
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
